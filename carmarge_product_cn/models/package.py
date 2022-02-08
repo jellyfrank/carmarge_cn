24cf1bcf0162846369c1c02160980204b8a9dc7e
Kevin Kong <kfx2007@outlook.com>
2022-02-08 21:50:20 +0800

    [IMP]调整

--- carmarge_product_cn/models/package.py
+++ carmarge_product_cn/models/package.py
@@ -13,7 +13,7 @@ class product_packaging(models.Model):
     def _get_volume(self):
         """计算体积"""
         for packaging in self:
-            packaging.volume = packaging.height * packaging.length * packaging.width
+            packaging.volume = packaging.height * packaging.length * packaging.width / 1000
 
     height = fields.Float("高")
     length = fields.Float("长")
