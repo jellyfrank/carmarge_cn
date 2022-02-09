9dda8b4284542012036aae64fd2a2368118cebd8
Kevin Kong <kfx2007@outlook.com>
2022-02-09 15:00:00 +0800

    [ADD]标签和唛头

--- carmarge_product_cn/__manifest__.py
+++ carmarge_product_cn/__manifest__.py
@@ -24,11 +24,13 @@
 
     # always loaded
     'data': [
+        'security/data.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/product.xml',
-        'views/packaging.xml'
+        'views/packaging.xml',
+        'views/report.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
