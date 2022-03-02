3b9cbfc7c1ccac5777a6c847cb0a3e5466e4eae8
Kevin Kong <kfx2007@outlook.com>
2022-03-02 14:06:32 +0800

    [FIX]合并功能优化

--- carmarge_product_cn/wizard/product_merge.py
+++ carmarge_product_cn/wizard/product_merge.py
@@ -4,9 +4,11 @@
 
 import datetime
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 import logging
 
 _logger = logging.getLogger(__name__)
+
 
 class MergeProductAutomatic(models.TransientModel):
     """
@@ -20,76 +22,105 @@ class MergeProductAutomatic(models.TransientModel):
         res = super(MergeProductAutomatic, self).default_get(fields)
         active_ids = self.env.context.get('active_ids')
         if self.env.context.get('active_model') == 'product.template' and active_ids:
-            if 'product_temp_ids' in fields:
-                res['product_temp_ids'] = [(6, 0, active_ids)]
+            if 'lines' in fields:
+                res['lines'] = [(0, 0, {"product_tmpl_id": product_tmpl_id})
+                                for product_tmpl_id in active_ids]
             if 'dst_product_temp_id' in fields:
-                res['dst_product_temp_id'] = self._get_dst_product(active_ids)[-1].id
+                res['dst_product_temp_id'] = self._get_dst_product(
+                    active_ids)[-1].id
         return res
 
     @api.model
     def _get_dst_product(self, partner_ids):
         # 计算第一个产品
         return self.env['product.template'].browse(partner_ids).sorted(
-            key=lambda p: (p.active, (p.create_date or datetime.datetime(1970, 1, 1))),
+            key=lambda p: (
+                p.active, (p.create_date or datetime.datetime(1970, 1, 1))),
             reverse=True,
         )
 
-    product_temp_ids = fields.Many2many('product.template', string='选中的产品')
     dst_product_temp_id = fields.Many2one('product.template', string='目标产品')
+    lines = fields.One2many(
+        "product.merge.automatic.wizard.line", "order_id", string="明细")
 
     def action_product_merge(self):
         """ 合并产品 """
-        try:
-            _logger.debug(f"[产品合并]开始合并产品")
-            purchases_count,sales_count, qty_available = 0, 0, 0 # 采购数量值 销售数量值 在手数量
-            product_ids_list,barcode_list = [],[]
-            for product_temp in self.product_temp_ids:
-                qty_available += product_temp.qty_available
-                product_ids_list.extend(product_temp.product_variant_ids.ids)
-                barcode_list.append(product_temp.barcode)
-                if product_temp != self.dst_product_temp_id:
-                    # 将归档产品的销售采购数量加总到保留产品上
-                    purchases_count += product_temp.purchased_product_qty
-                    sales_count += product_temp.sales_count
-                    # 将除目标产品外的产品进行归档
-                    product_temp.active = False
-            # 将归档产品的采购数量加总到保留产品上
-            data = {
-                "other_purchases_count": purchases_count,
-                "other_sales_count": sales_count,
-                "merge_temp_ids": ','.join(list(map(lambda val: str(val), self.product_temp_ids.ids))),
+        _logger.debug(f"[产品合并]开始合并产品")
+        purchases_count, sales_count, qty_available = 0, 0, 0  # 采购数量值 销售数量值 在手数量
+        product_ids_list, barcode_list = [], []
+        print( self.lines)
+        for line in self.lines:
+            product_temp = line.product_tmpl_id
+            qty_available += product_temp.qty_available
+            product_ids_list.extend(product_temp.product_variant_ids.ids)
+            barcode_list.append(product_temp.barcode)
+            if product_temp != self.dst_product_temp_id:
+                # 将归档产品的销售采购数量加总到保留产品上
+                purchases_count += product_temp.purchased_product_qty
+                sales_count += product_temp.sales_count
+                # 将除目标产品外的产品进行归档
+                product_temp.active = False
+        # 将归档产品的采购数量加总到保留产品上
+        data = {
+            "other_purchases_count": purchases_count,
+            "other_sales_count": sales_count,
+            "merge_temp_ids": ','.join(str(p.id) for p in self.lines.product_tmpl_id)
+        }
+        # 如果目标产品有条码 就用目标产品的条码 如果没有 就选最小的那个
+        if not self.dst_product_temp_id.barcode:
+            data['barcode'] = sorted(barcode_list)[
+                0] if barcode_list else None
+        else:
+            data['barcode'] = self.dst_product_temp_id.barcode
+        _logger.debug(f"[产品合并]产品合并数据:{data}")
+        self.dst_product_temp_id.write(data)
+        # 将在手数量进行更新
+        inventory_obj = self.env["stock.inventory"].create({
+            'product_ids': [(6, 0, product_ids_list)],
+        })
+        # 复用系统原生库存盘点逻辑
+        for inventory in inventory_obj:
+            if inventory.state != 'draft':
+                continue
+            vals = {
+                'state': 'confirm',
+                'date': fields.Datetime.now()
             }
-            # 如果目标产品有条码 就用目标产品的条码 如果没有 就选最小的那个
-            if not self.dst_product_temp_id.barcode:
-                data['barcode'] = sorted(barcode_list)[0] if barcode_list else None
-            else:
-                data['barcode'] = self.self.dst_product_temp_id.barcode
-            _logger.debug(f"[产品合并]产品合并数据:{data}")
-            self.dst_product_temp_id.write(data)
-            # 将在手数量进行更新
-            inventory_obj = self.env["stock.inventory"].create({
-                'product_ids': [(6, 0, product_ids_list)],
-            })
-            # 复用系统原生库存盘点逻辑
-            for inventory in inventory_obj:
-                if inventory.state != 'draft':
-                    continue
-                vals = {
-                    'state': 'confirm',
-                    'date': fields.Datetime.now()
-                }
-                if not inventory.line_ids and not inventory.start_empty:
-                    line_values = inventory._get_inventory_lines_values()
+            if not inventory.line_ids and not inventory.start_empty:
+                line_values = inventory._get_inventory_lines_values()
+                if line_values:
                     # 将所有的产品进行盘点(在不修改原生逻辑的情况下)
                     for line in line_values:
                         if line['product_id'] not in self.dst_product_temp_id.product_variant_ids.ids:
                             line['product_qty'] = 0
                         else:
                             line['product_qty'] = qty_available
-                    self.env['stock.inventory.line'].create(line_values)
-                inventory.write(vals)
-            inventory_obj._check_company()
-            # 验证盘点
-            inventory_obj.action_validate()
-        except Exception as err:
-            _logger.exception(f"合并产品异常")
+                else:
+                    location_id = self.env['stock.location'].search([('usage','=','internal')],limit=1)
+                    line_values = {
+                        "inventory_id": inventory.id,
+                        "product_id": self.dst_product_temp_id.product_variant_id.id,
+                        "product_qty": qty_available,
+                        "location_id": location_id.id,
+                        "company_id": inventory.company_id.id
+                    }
+                    print(line_values)
+                self.env['stock.inventory.line'].create(line_values)
+            inventory.write(vals)
+        # inventory_obj._action_start()
+        inventory_obj._check_company()
+        # 验证盘点
+        inventory_obj.action_validate()
+
+
+class MergeProductAutomaticLine(models.TransientModel):
+    _name = "product.merge.automatic.wizard.line"
+
+    order_id = fields.Many2one("product.merge.automatic.wizard")
+    product_tmpl_id = fields.Many2one('product.template', string='选中的产品')
+    categ_id = fields.Many2one(
+        "product.category", related="product_tmpl_id.categ_id")
+    name = fields.Char("name", related="product_tmpl_id.name")
+    default_code = fields.Char(
+        "default_code", related="product_tmpl_id.default_code")
+    barcode = fields.Char("barcode", related="product_tmpl_id.barcode")
