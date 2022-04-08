4597e0ff00713ffb498b149c7ff0d91c4e55e74e
lkw <2796047438@qq.com>
2022-04-08 10:16:37 +0800

    国内需求第八期修改

---
+++ carmarge_sale_cn/models/account_move.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from odoo import api, fields, models, _
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_sale_order_amount(self):
+        '''
+        获取销售订单的总金额
+        '''
+        self.ensure_one()
+        report_arr = []
+        sale_order = self.env['sale.order'].search([('name','=',self.invoice_origin)])
+        if sale_order:
+            report_arr.append({
+                'amount_total':sale_order.amount_total,
+                'amount_untaxed':sale_order.amount_untaxed,
+                'amount_tax':sale_order.amount_tax,
+                'delivery_cost':sale_order.delivery_cost,
+            })
+        return report_arr
+
+
+    def _get_deposit_default_product_id(self):
+        product = self.env['ir.config_parameter'].sudo().get_param('sale.default_deposit_product_id')
+        product_id = self.env['product.product'].browse(int(product)).exists()
+        return product_id.id
