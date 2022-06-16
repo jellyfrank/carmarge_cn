34ca6fc838014f5b78705e088d82a0d8734e25fe
LIU jiang <jiang.liu@hexagon.com>
2022-06-16 08:59:17 +0800

    ✨ feat(partner): update form
    - update inactive label
    - remove street2

--- carmarge_partner_cn/models/res_partner.py
+++ carmarge_partner_cn/models/res_partner.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    partner_type = fields.Selection(string='联系人类型', selection=[('distributor', '汽配经销商'), ('fleet', '车队'), ('supplier','供应商')],default='distributor')
+    partner_type = fields.Selection(string='联系人类型', selection=[('distributor', '经销商'), ('fleet', '车队'), ('supplier','供应商')],default='distributor')
