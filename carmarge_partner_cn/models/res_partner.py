907a14ed5ea58e0e1ac73672c09e036ab03f2c1a
LIU jiang <jiang.liu@hexagon.com>
2022-06-16 16:36:33 +0800

    ✨ feat(partner): add partner_type to menu

--- carmarge_partner_cn/models/res_partner.py
+++ carmarge_partner_cn/models/res_partner.py
@@ -6,4 +6,15 @@ from odoo import models, fields, api
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    partner_type = fields.Selection(string='联系人类型', selection=[('distributor', '经销商'), ('fleet', '车队'), ('supplier','供应商')],default='distributor')
+    partner_type = fields.Selection(string='联系人类型', selection=[(
+        'distributor', '经销商'), ('fleet', '终端客户'), ('supplier', '供应商')])
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        value = self.env.context.get('partner_type')
+        newValue = 'distributor' if value == None else value
+        res.update({
+            'partner_type': newValue,
+        })
+        return res
