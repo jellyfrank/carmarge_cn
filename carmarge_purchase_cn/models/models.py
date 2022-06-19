9b3f908b7598a119d6cbdd3836cf6fef613a746c
Kevin Kong <kfx2007@outlook.com>
2022-06-19 22:40:53 +0800

    [ADD]采购添加供应商时带入数量

--- carmarge_purchase_cn/models/models.py
+++ carmarge_purchase_cn/models/models.py
@@ -15,9 +15,9 @@ class purchase_order(models.Model):
     def _amount_all(self):
         super(purchase_order, self)._amount_all()
         delivery_product_id = self.env.ref(
-            "carmarge_sale_cn.service_delivery_cost")
+            "carmarge_purchase_cn.service_delivery_cost")
         discount_product_id = self.env.ref(
-            "carmarge_sale_cn.service_discount")
+            "carmarge_purchase_cn.service_discount")
         # 添加运费和优惠
         for order in self:
             amount_untaxed = amount_tax = 0.0
@@ -59,9 +59,9 @@ class purchase_order(models.Model):
         """计算海运费和优惠"""
         for po in self:
             delivery_product_id = po.env.ref(
-                "carmarge_sale_cn.service_delivery_cost")
+                "carmarge_purchase_cn.service_delivery_cost")
             discount_product_id = po.env.ref(
-                "carmarge_sale_cn.service_discount")
+                "carmarge_purchase_cn.service_discount")
             if not delivery_product_id:
                 po.delivery_cost = 0
             if not discount_product_id:
@@ -80,6 +80,49 @@ class purchase_order(models.Model):
             discount_line = po.order_line.filtered(
                 lambda l: l.product_id == discount_product_id.product_variant_id)
             po.discount_manual = discount_line.price_subtotal
+
+    def _add_supplier_to_product(self):
+        # Add the partner in the supplier list of the product if the supplier is not registered for
+        # this product. We limit to 10 the number of suppliers for a product to avoid the mess that
+        # could be caused for some generic products ("Miscellaneous").
+        for line in self.order_line:
+            # Do not add a contact as a supplier
+            partner = self.partner_id if not self.partner_id.parent_id else self.partner_id.parent_id
+            if line.product_id and partner not in line.product_id.seller_ids.mapped('name') and len(line.product_id.seller_ids) <= 10:
+                # Convert the price in the right currency.
+                currency = partner.property_purchase_currency_id or self.env.company.currency_id
+                price = self.currency_id._convert(line.price_unit, currency, line.company_id, line.date_order or fields.Date.today(), round=False)
+                # Compute the price for the template's UoM, because the supplier's UoM is related to that UoM.
+                if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
+                    default_uom = line.product_id.product_tmpl_id.uom_po_id
+                    price = line.product_uom._compute_price(price, default_uom)
+
+                supplierinfo = {
+                    'name': partner.id,
+                    'sequence': max(line.product_id.seller_ids.mapped('sequence')) + 1 if line.product_id.seller_ids else 1,
+                    'min_qty': 0.0,
+                    'price': price,
+                    'currency_id': currency.id,
+                    'delay': 0,
+                    "min_qty": line.product_qty
+                }
+                # In case the order partner is a contact address, a new supplierinfo is created on
+                # the parent company. In this case, we keep the product name and code.
+                seller = line.product_id._select_seller(
+                    partner_id=line.partner_id,
+                    quantity=line.product_qty,
+                    date=line.order_id.date_order and line.order_id.date_order.date(),
+                    uom_id=line.product_uom)
+                if seller:
+                    supplierinfo['product_name'] = seller.product_name
+                    supplierinfo['product_code'] = seller.product_code
+                vals = {
+                    'seller_ids': [(0, 0, supplierinfo)],
+                }
+                try:
+                    line.product_id.write(vals)
+                except AccessError:  # no write access rights -> just ignore
+                    break
 
     delivery_cost = fields.Monetary("运费",compute="_compute_delivery_discount", store=True)
     discount_manual = fields.Monetary("优惠",compute="_compute_delivery_discount", store=True)
