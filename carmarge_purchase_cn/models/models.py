5955c5cae3c2a956aad8931a7eb4d8ed6ffa90aa
Kevin Kong <kfx2007@outlook.com>
2024-01-17 14:36:37 +0800

    [IMP] #117 单价不随数量变化而变化

--- carmarge_purchase_cn/models/models.py
+++ carmarge_purchase_cn/models/models.py
@@ -184,59 +184,8 @@ class purchase_order_line(models.Model):
 
     @api.onchange('product_qty', 'product_uom', 'company_id')
     def _onchange_quantity(self):
-        if not self.product_id or self.invoice_lines or not self.company_id:
-            return
-        params = {'order_id': self.order_id}
-        seller = self.product_id._select_seller(
-            partner_id=self.partner_id,
-            quantity=self.product_qty,
-            date=self.order_id.date_order and self.order_id.date_order.date(),
-            uom_id=self.product_uom,
-            params=params)
-
-        if seller or not self.date_planned:
-            self.date_planned = self._get_date_planned(seller).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-
-        # If not seller, use the standard price. It needs a proper currency conversion.
-        if not seller:
-            po_line_uom = self.product_uom or self.product_id.uom_po_id
-            price_unit = self.env['account.tax']._fix_tax_included_price_company(
-                self.product_id.uom_id._compute_price(self.product_id.standard_price, po_line_uom),
-                self.product_id.supplier_taxes_id,
-                self.taxes_id,
-                self.company_id,
-            )
-            if price_unit and self.order_id.currency_id and self.order_id.company_id.currency_id != self.order_id.currency_id:
-                price_unit = self.order_id.company_id.currency_id._convert(
-                    price_unit,
-                    self.order_id.currency_id,
-                    self.order_id.company_id,
-                    self.date_order or fields.Date.today(),
-                )
-
-            # self.price_unit = price_unit
-            return
-
-        price_unit = self.env['account.tax']._fix_tax_included_price_company(seller.price, self.product_id.supplier_taxes_id, self.taxes_id, self.company_id) if seller else 0.0
-
-        if price_unit and seller and self.order_id.currency_id and seller.currency_id != self.order_id.currency_id:
-            price_unit = seller.currency_id._convert(
-                price_unit, self.order_id.currency_id, self.order_id.company_id, self.date_order or fields.Date.today())
-
-        if seller and self.product_uom and seller.product_uom != self.product_uom:
-            price_unit = seller.product_uom._compute_price(price_unit, self.product_uom)
-
-        # self.price_unit = price_unit
-
-        default_names = []
-        vendors = self.product_id._prepare_sellers({})
-        for vendor in vendors:
-            product_ctx = {'seller_id': vendor.id, 'lang': get_lang(self.env, self.partner_id.lang).code}
-            default_names.append(self._get_product_purchase_description(self.product_id.with_context(product_ctx)))
-
-        if (self.name in default_names or not self.name):
-            product_ctx = {'seller_id': seller.id, 'lang': get_lang(self.env, self.partner_id.lang).code}
-            self.name = self._get_product_purchase_description(self.product_id.with_context(product_ctx))
+        if self._context.get("changed"):
+            super()._onchange_quantity()
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -280,6 +229,7 @@ class purchase_order_line(models.Model):
         '''
         根据产品获取包装规格
         '''
+        self.with_context({'changed': True})._onchange_quantity()
         product_ids = []
         self.packaging=False
         product_data = self.product_id
